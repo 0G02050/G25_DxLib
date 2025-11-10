c84addd738c05b3538dbbc20252949be69f83d77
ogawamasahiro <ogawamasahiro@P705>
2025-11-10 10:58:34 +0900

    プロジェクト ファイルを追加します。

---
+++ test.cpp
@@ -0,0 +1,20 @@
+include "DxLib.h"
+
+// プログラムは WinMain から始まります
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	ChangeWindowMode(TRUE);		// ウィンドウモードで起動
+
+	if (DxLib_Init() == -1)		// ＤＸライブラリ初期化処理
+	{
+		return -1;			// エラーが起きたら直ちに終了
+	}
+
+	DrawBox(200, 200, 400, 400, GetColor(255, 255, 255), FALSE);	// 四角形表示
+
+	WaitKey();				// キー入力待ち
+
+	DxLib_End();				// ＤＸライブラリ使用の終了処理
+
+	return 0;				// ソフトの終了 
+}
